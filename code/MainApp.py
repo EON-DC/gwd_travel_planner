e9bd1de5fe8bd5460e3f8a7d94a542f660bb76bf
gwang <mun0392@gmail.com>
2023-07-04 09:36:52 +0900

    init

--- code/MainApp.py
+++ code/MainApp.py
@@ -1,5 +1,30 @@
+# 배포전 확인!
+# UI 파일 삭제, database main 버전
+
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sqlite3
+import pandas as pd
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+import folium
+
 def main():
-    pass
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = QtWidgets.QMainWindow()
+    main_window.show()
+
+    sys.excepthook = lambda exctype, value, traceback: show_error_message(str(value), traceback)
+
+    app.exec_()
+
+
+def show_error_message(message, traceback):
+    msg_box = QtWidgets.QMessageBox()
+    msg_box.setIcon(QtWidgets.QMessageBox.Critical)
+    msg_box.setWindowTitle("Error")
+    msg_box.setText(message)
+    msg_box.exec_()
+    traceback.print_exc()
 
 
 if __name__ == '__main__':
