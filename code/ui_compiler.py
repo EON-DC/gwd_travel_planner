50a6eb15bf100aa9d350446b29b6fc5f86d348c9
kdt100 <alfajeze@gmail.com>
2023-07-04 21:15:41 +0900

    csv파일 추가

--- code/ui_compiler.py
+++ code/ui_compiler.py
@@ -1,6 +1,6 @@
 import os
 
 if __name__ == '__main__':
-    os.system('pyuic5 ui/ui_login_widget.ui -o ui_login_widget.py')
-    os.system('pyuic5 ui/ui_vital_widget.ui -o ui_vital_widget.py')
-    os.system('pyuic5 ui/ui_patient_finder.ui -o ui_patient_finder.py')
+    os.system('pyuic5 sample.ui -o sample.py')
+    # os.system('pyuic5 ui/ui_vital_widget.ui -o ui_vital_widget.py')
+    # os.system('pyuic5 ui/ui_patient_finder.ui -o ui_patient_finder.py')
